d101808dc211826d03e16e3bdd1dd35ec3e7997c
mgraupe <michael.graupner@parisdescartes.fr>
2019-11-11 21:40:05 +0100

    generated summary data

--- runAllScripts.py
+++ runAllScripts.py
@@ -7,8 +7,8 @@ animals = ['animal#1','animal#3','animal#2','animal#4','animal#1_2']
 
 for a in animals:
     print('analyzing animal %s' % a)
-    #os.system('python alignImages.py %s' % a)
-    #os.system('python matchROIs.py %s' % a)
+    os.system('python alignImages.py %s' % a)
+    os.system('python matchROIs.py %s' % a)
     os.system('python analyzeFluoTraces.py %s' % a)
 
 
